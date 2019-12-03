17c345237ff52de04ffb6bea814f4c975339ed6c
decoejz <decoejz@gmail.com>
2019-12-03 00:10:47 -0300

    contract done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,7 +1,8 @@
 pragma solidity >=0.4.2 <0.7.0;
 
-//Referencia para o codigo:
-//https://www.dappuniversity.com/articles/the-ultimate-ethereum-dapp-tutorial
+// Referencia para o codigo:
+// https://www.dappuniversity.com/articles/the-ultimate-ethereum-dapp-tutorial
+// https://ethereum.stackexchange.com/questions/65589/return-a-mapping-in-a-getall-function
 
 //truffle migrate --reset
 //truffle console
@@ -50,14 +51,14 @@ contract Election {
     // Inicializa a eleicao com os candidatos
     constructor () public {
         criador = msg.sender;
-        addCandidato("Manoela","Mais Cloud",address(0xf9Ff6C8fBDf4bed17C5c2E4504e22fCe3225F1eD));
-        addCandidato("Wesley","Mais Raspberry Pi",address(0x20d686Cb5b4e282614BC08E02917E2fba6d32296));
-        addCandidato("David","Mais Node / React",address(0xA1DaBAe16616526F08E921FB1D03C2814De32b68));
-        addEleitor(address(0x617DeD1392499354fD091F1F2c6Eb71e8b937CbC));
-        addEleitor(address(0xe76B54d33B9F6f2f35e51708c0C94511CEaC9098));
-        addEleitor(address(0x35E55be62Bd30be203090DfA12F26490e55Ec779));
-        addEleitor(address(0xf9Ff6C8fBDf4bed17C5c2E4504e22fCe3225F1eD));
-        addEleitor(address(0x20d686Cb5b4e282614BC08E02917E2fba6d32296));
+        addCandidato("Manoela","Mais Cloud",address(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c));
+        addCandidato("Wesley","Mais Raspberry Pi",address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C));
+        addCandidato("David","Mais Node / React",address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB));
+        addEleitor(address(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c));
+        addEleitor(address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C));
+        addEleitor(address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB));
+        addEleitor(address(0x583031D1113aD414F02576BD6afaBfb302140225));
+        addEleitor(address(0xdD870fA1b7C4700F2BD7f44238821C26f7392148));
     }
 
     event eventoVotacaoCandidato (
@@ -68,25 +69,25 @@ contract Election {
         //Garante que apenas o criador do contrato (eleicao) possa adicionar novos eleitores
         require(criador == msg.sender, "Somente o criador do contrato pode adicionar novos eleitores");
         
-        // Cria um novo eleitor
-        Eleitores[_conta] = Eleitor(totalEleitores, _conta, false);
         // Adiciona 1 nos valores de id
         totalEleitores ++;
+        // Cria um novo eleitor
+        Eleitores[_conta] = Eleitor(totalEleitores, _conta, false);
     }
 
     function addCandidato (string memory _name, string memory _proposta, address _conta) private {
         //Garante que apenas o criador do contrato (eleicao) possa adicionar novos candidatos
         require(criador == msg.sender, "Somente o criador do contrato pode adicionar novos candidatos");
         
-        // Cria um novo candidato
-        Candidatos[totalCandidatos] = Candidato(totalCandidatos-1, _conta, _name, _proposta, 0);
         // Adiciona 1 nos valores de id
         totalCandidatos ++;
+        // Cria um novo candidato
+        Candidatos[totalCandidatos] = Candidato(totalCandidatos-1, _conta, _name, _proposta, 0);
     }
 
     function vote (uint _CandidatoId) public {
         // Garante que o eleitor esta na lista de eleitores
-        require(Eleitores[msg.sender].id >= 0 && Eleitores[msg.sender].id < totalEleitores, "Você não tem direito de votar");
+        require(Eleitores[msg.sender].id > 0 && Eleitores[msg.sender].id <= totalEleitores, "Você não tem direito de votar");
 
         // Garante que a pessoa ainda não votou
         // Caso ja tinha votado, ele sai da funcao
@@ -94,7 +95,7 @@ contract Election {
 
         // Garante que o candidato exista
         // Caso o candidato nao exista, ele sai da funcao
-        require(_CandidatoId >= 0 && _CandidatoId < totalCandidatos, "Candidato inexistente");
+        require(_CandidatoId > 0 && _CandidatoId <= totalCandidatos, "Candidato inexistente");
 
         // Muda o estado do eleitor dizendo que ele votou
         Eleitores[msg.sender].votou = true;
@@ -114,7 +115,7 @@ contract Election {
     function vencedor() public view returns (string memory nome, uint totalVotos){
         // Retorna o nome e a quantidade de votos do vencedor da eleicao
         totalVotos = 0;
-        for (uint p = 0; p < totalCandidatos; p++) {
+        for (uint p = 1; p <= totalCandidatos; p++) {
             if (Candidatos[p].contagem > totalVotos) {
                 totalVotos = Candidatos[p].contagem;
                 nome = Candidatos[p].nome;
@@ -131,7 +132,7 @@ contract Election {
         // Retorna uma lista com todos os ids de 
         address[] memory ret = new address[](totalCandidatos);
         for (uint i = 0; i < totalCandidatos; i++) {
-            ret[i] = Candidatos[i].conta;
+            ret[i] = Candidatos[i+1].conta;
         }
         return ret;
     }
